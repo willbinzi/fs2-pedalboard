bc7a450ed386ab0f67f0f904b6df78009545f234
Will Binzi <will.binzi@moia.io>
2023-09-29 23:38:44 +0200

    Add tremolo implementations

--- src/main/scala/pedals/overdrive.scala
+++ src/main/scala/pedals/overdrive.scala
@@ -1,4 +1,7 @@
 package pedals
 
-def overdrive[F[_]]: Pedal[F] = 
-  Pedal(sample => math.min(sample, -0.5f))
+def overdrive[F[_]](threshold: Float): Pedal[F] =
+  Pedal(sample => math.min(math.max(sample, -threshold), threshold))
+
+def asymmetricOverdrive[F[_]](threshold: Float): Pedal[F] =
+  Pedal(sample => math.min(sample, threshold))
--- src/main/scala/pedals/tremolo.scala
+++ src/main/scala/pedals/tremolo.scala
@@ -1,4 +1,45 @@
 package pedals
 
-def sineWave(timestamp: Int, cycleLength: Double, frameRate: Float): Float =
-  Math.sin(timestamp / cycleLength * 2 * Math.PI).toFloat
+import fs2.{ Chunk, Pull, Pure, Stream }
+import config.{ AUDIO_FORMAT, FLOAT_BUFFER_SIZE }
+
+val CHUNKS_PER_SECOND: Float = AUDIO_FORMAT.getSampleRate / FLOAT_BUFFER_SIZE
+
+def waveSection(sectionLengthInChunks: Int, f: Long => Float): Stream[Pure, Float] =
+  val buffer = new Array[Float](FLOAT_BUFFER_SIZE)
+
+  def go(chunkNumber: Long): Pull[Pure, Float, Option[Long]] =
+    for (i <- 0 until FLOAT_BUFFER_SIZE) do
+      buffer(i) = f((chunkNumber * FLOAT_BUFFER_SIZE) + i)
+    Pull.output(Chunk.array(buffer)) >> (
+      if chunkNumber < sectionLengthInChunks then
+        Pull.pure(Some(chunkNumber + 1))
+      else
+        Pull.pure(None)
+    )
+  Pull.loop[Pure, Float, Long](go)(0).stream
+
+def triangleWave(cycleLengthInSeconds: Float): Stream[Pure, Float] =
+  val slopeLengthInChunks = (cycleLengthInSeconds * CHUNKS_PER_SECOND).toInt
+  val slopeLengthInFrames = slopeLengthInChunks * FLOAT_BUFFER_SIZE
+  (waveSection(slopeLengthInChunks, slope(slopeLengthInFrames)) ++
+    waveSection(slopeLengthInChunks, x => 1 - slope(slopeLengthInFrames)(x))).repeat
+
+def sineWave(cycleLengthInSeconds: Float): Stream[Pure, Float] =
+  val slopeLengthInChunks = (2 * cycleLengthInSeconds * CHUNKS_PER_SECOND).toInt
+  val slopeLengthInFrames = slopeLengthInChunks * FLOAT_BUFFER_SIZE
+  waveSection(slopeLengthInChunks, sine(slopeLengthInFrames)).repeat
+
+def sine(cycleLengthInFrames: Long)(timestamp: Long): Float =
+  (1 + math.sin(timestamp.toDouble * 2 * math.Pi / cycleLengthInFrames).toFloat) / 2
+
+def slope(slopeLengthInFrames: Long)(timestamp: Long): Float =
+  timestamp / (slopeLengthInFrames).toFloat
+
+def squareWave(cycleLengthInSeconds: Float): Stream[Pure, Float] =
+  val halfCycleLengthInChunks = (cycleLengthInSeconds * CHUNKS_PER_SECOND).toInt
+  (waveSection(halfCycleLengthInChunks, _ => 1) ++
+    waveSection(halfCycleLengthInChunks, _ => 0)).repeat
+
+def tremolo[F[_]](cycleLengthInSeconds: Float): Pedal[F] =
+  _.zipWith(sineWave(cycleLengthInSeconds))(_ * _)
