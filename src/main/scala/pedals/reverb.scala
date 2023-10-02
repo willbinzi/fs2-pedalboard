77c6f97093a46825329770c452da6033c8eec973
Will Binzi <will.binzi@moia.io>
2023-10-02 22:37:34 +0200

    Play around with delay order in reverb

--- src/main/scala/pedals/reverb.scala
+++ src/main/scala/pedals/reverb.scala
@@ -8,27 +8,29 @@ import fs2.Chunk
 
 def reverbR[F[_]: Concurrent]: Resource[F, Pedal[F]] =
   for {
-    topic   <- Topic[F, Chunk[Float]].toResource
-    stream1 <- topic.subscribeAwaitUnbounded.map(_.unchunks)
-    stream2 <- topic.subscribeAwaitUnbounded.map(_.unchunks)
-    stream3 <- topic.subscribeAwaitUnbounded.map(_.unchunks)
-    stream4 <- topic.subscribeAwaitUnbounded.map(_.unchunks)
-    delay1  <- delayF(0.742, 4.799).toResource
-    delay2  <- delayF(0.733, 4.999).toResource
-    delay3  <- delayF(0.715, 5.399).toResource
-    delay4  <- delayF(0.697, 5.801).toResource
+    topic    <- Topic[F, Chunk[Float]].toResource
+    stream1  <- topic.subscribeAwaitUnbounded.map(_.unchunks)
+    stream2  <- topic.subscribeAwaitUnbounded.map(_.unchunks)
+    stream3  <- topic.subscribeAwaitUnbounded.map(_.unchunks)
+    stream4  <- topic.subscribeAwaitUnbounded.map(_.unchunks)
+    delay1   <- delayF(0.742, 4.799).toResource
+    delay2   <- delayF(0.733, 4.999).toResource
+    delay3   <- delayF(0.715, 5.399).toResource
+    delay4   <- delayF(0.697, 5.801).toResource
     allPass1 <- allPassFilterF(0.7, 1.051).toResource
     allPass2 <- allPassFilterF(0.7, 0.337).toResource
+    allPass3 <- allPassFilterF(0.7, 0.113).toResource
   } yield (
-    stream =>
-      stream.chunks.evalMap( sampleChunk =>
-        topic.publish1(sampleChunk)
-      ).zipRight(
+    _
+      .through(allPass1)
+      .through(allPass2)
+      .through(allPass3)
+      .chunks
+      .evalMap(topic.publish1)
+      .zipRight(
         stream1.through(delay1).chunks |+|
         stream2.through(delay2).chunks |+|
         stream3.through(delay3).chunks |+|
         stream4.through(delay4).chunks
       ).unchunks
-      .through(allPass1)
-      .through(allPass2)
   )
