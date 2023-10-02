c8c2989c7bcea24cf80b18b9447af74b0af0db21
Will Binzi <will.binzi@moia.io>
2023-10-02 22:06:56 +0200

    Implement all pass filter

--- src/main/scala/pedals/delay.scala
+++ src/main/scala/pedals/delay.scala
@@ -5,6 +5,7 @@ import constants.{ CHUNKS_PER_SECOND, FLOAT_BUFFER_SIZE }
 import fs2.concurrent.Channel
 import fs2.{ Chunk, Stream }
 import cats.syntax.functor.*
+import cats.syntax.flatMap.*
 import cats.syntax.semigroup.*
 
 def delay[F[_]](delayTimeInSeconds: Float, feedback: Float): Pedal[F] =
@@ -28,7 +29,22 @@ def delayF[F[_]: Concurrent](repeatGain: Float, delayTimeInSeconds: Float): F[Pe
     stream =>
       (
         stream.chunks |+|
-          (silence(delayTimeInSeconds) ++ repeatsChannel.stream.map(_.map(_ * repeatGain)))
+          (silence(delayTimeInSeconds) ++ repeatsChannel.stream.map(_ * repeatGain))
       ).evalTap(repeatsChannel.send)
       .unchunks
   )
+
+def allPassFilterF[F[_]: Concurrent](repeatGain: Float, delayTimeInSeconds: Float): F[Pedal[F]] =
+  for {
+    outputCopyChannel <- Channel.unbounded[F, Chunk[Float]]
+    inputCopyChannel <- Channel.unbounded[F, Chunk[Float]]
+  } yield {
+    stream =>
+      (stream.chunks
+        .evalTap(inputCopyChannel.send)
+        .map(_ * (-repeatGain)) |+|
+          (silence(delayTimeInSeconds) ++ inputCopyChannel.stream) |+|
+          (silence(delayTimeInSeconds) ++ outputCopyChannel.stream.map(_ * repeatGain))
+      ).evalTap(outputCopyChannel.send)
+      .unchunks
+  }
--- src/main/scala/pedals/package.scala
+++ src/main/scala/pedals/package.scala
@@ -5,5 +5,12 @@ package object pedals {
   implicit def streamPointwiseAddChunks[F[_]]: Semigroup[Stream[F, Chunk[Float]]] =
     new Semigroup[Stream[F, Chunk[Float]]]:
       def combine(x: Stream[F, Chunk[Float]], y: Stream[F, Chunk[Float]]): Stream[F, Chunk[Float]] =
-        x.zipWith(y)(_.zip(_).map(_ + _))
+        x.zipWith(y)(_ pointWiseAdd _)
+
+  extension (chunk: Chunk[Float])
+    def pointWiseAdd(other: Chunk[Float]): Chunk[Float] =
+      chunk.zipWith(other)(_ + _)
+
+    def * (scalar: Float): Chunk[Float] =
+      chunk.map(_ * scalar)
 }
