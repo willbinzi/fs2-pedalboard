fd72238a9c198a1d8ccf65b6e3038b92806c6930
Will Binzi <will.binzi@moia.io>
2023-10-04 09:40:46 +0200

    Add overdride with blended clean signal

--- src/main/scala/pedals/overdrive.scala
+++
@@ -1,7 +0,0 @@
-package pedals
-
-def overdrive[F[_]](threshold: Float): Pedal[F] =
-  _.map(sample => math.min(math.max(sample, -threshold), threshold))
-
-def asymmetricOverdrive[F[_]](threshold: Float): Pedal[F] =
-  _.map(sample => math.min(sample, threshold))
---
+++ src/main/scala/pedals/overdrive/package.scala
@@ -0,0 +1,18 @@
+package pedals
+package overdrive
+
+import cats.effect.kernel.Resource
+import cats.effect.Concurrent
+import pedals.routing.parallel
+
+def asymmetricClipping[F[_]](threshold: Float): Pedal[F] =
+  _.map(sample => math.min(sample, threshold))
+
+def symmetricClipping[F[_]](threshold: Float): Pedal[F] =
+  _.map(sample => math.min(math.max(sample, -threshold), threshold))
+
+def blended[F[_]: Concurrent](overdriveFactor: Float, threshold: Float): Resource[F, Pedal[F]] =
+  parallel(
+    passThrough andThen (_.map(_ * (1 - overdriveFactor))),
+    asymmetricClipping(threshold) andThen (_.map(_ * overdriveFactor))
+  )
