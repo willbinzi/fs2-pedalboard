4447c58f9b457f01e117f6fceb96b726fc6a3b84
Will Binzi <will.binzi@moia.io>
2023-10-23 09:25:49 +0200

    Reorganise constants a tad

--- src/main/scala/pedals/tremolo/waveforms/util/waveSection.scala
+++ src/main/scala/pedals/tremolo/waveforms/util/waveSection.scala
@@ -1,17 +1,17 @@
 package pedals.tremolo.waveforms.util
 
-import constants.FLOAT_BUFFER_SIZE
+import constants.FRAMES_PER_BUFFER
 import fs2.{Chunk, Pull, Pure, Stream}
 
 def waveSection(
     sectionLengthInChunks: Int,
     f: Long => Float
 ): Stream[Pure, Float] =
-  val buffer = new Array[Float](FLOAT_BUFFER_SIZE)
+  val buffer = new Array[Float](FRAMES_PER_BUFFER)
 
   def go(chunkNumber: Long): Pull[Pure, Float, Option[Long]] =
-    for (i <- 0 until FLOAT_BUFFER_SIZE) do
-      buffer(i) = f((chunkNumber * FLOAT_BUFFER_SIZE) + i)
+    for (i <- 0 until FRAMES_PER_BUFFER) do
+      buffer(i) = f((chunkNumber * FRAMES_PER_BUFFER) + i)
     Pull.output(Chunk.array(buffer)) >> (
       if chunkNumber < sectionLengthInChunks then
         Pull.pure(Some(chunkNumber + 1))
