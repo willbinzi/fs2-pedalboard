b432bd0a4143aab001d81dd8594973e9bcd0979f
Will Binzi <will.binzi@moia.io>
2025-02-14 19:20:15 +0100

    Read input stream directly into buffer used for chunk

--- src/main/scala/io/input.scala
+++ src/main/scala/io/input.scala
@@ -12,18 +12,12 @@ import scala.scalanative.unsigned.UnsignedRichInt
 def inputStreamFromPointer[F[_]](pStream: Ptr[PaStream])(implicit
     F: Sync[F]
 ): Stream[F, Float] =
-  val pFloat: Ptr[Float] = stackalloc[Float](FRAMES_PER_BUFFER)
-  val pByte: Ptr[Byte] = pFloat.toBytePointer
+  val buffer = new Array[Float](FRAMES_PER_BUFFER)
   Pull
     .eval(F.blocking {
-      functions.Pa_ReadStream(pStream, pByte, FRAMES_PER_BUFFER.toULong)
-      arrayChunk(pFloat, FRAMES_PER_BUFFER)
+      functions.Pa_ReadStream(pStream, buffer.atUnsafe(0).toBytePointer, FRAMES_PER_BUFFER.toULong)
+      Chunk.ArraySlice(buffer, 0, FRAMES_PER_BUFFER)
     })
     .flatMap(Pull.output)
     .streamNoScope
     .repeat
-
-private def arrayChunk(pointer: Ptr[Float], length: Int): Chunk[Float] =
-  val array = new Array[Float](length)
-  (0 until length).foreach(i => array(i) = pointer(i))
-  Chunk.array(array)
