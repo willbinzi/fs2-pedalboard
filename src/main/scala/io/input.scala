931f248e01373c6d5902169ad8349cea9e5b032e
Will Binzi <will.binzi@moia.io>
2025-02-14 17:22:59 +0100

    Use memcpy to copy memory more efficiently

--- src/main/scala/io/input.scala
+++ src/main/scala/io/input.scala
@@ -1,11 +1,12 @@
 package io
 
 import cats.effect.Sync
-import constants.FRAMES_PER_BUFFER
+import constants.{ BYTES_PER_BUFFER, FRAMES_PER_BUFFER }
 import fs2.{Chunk, Pull, Stream}
 import portaudio.aliases.PaStream
 import portaudio.functions
 
+import scala.scalanative.libc.string.memcpy
 import scala.scalanative.unsafe.*
 import scala.scalanative.unsigned.UnsignedRichInt
 
@@ -17,13 +18,13 @@ def inputStreamFromPointer[F[_]](pStream: Ptr[PaStream])(implicit
   Pull
     .eval(F.blocking {
       functions.Pa_ReadStream(pStream, pByte, FRAMES_PER_BUFFER.toULong)
-      arrayChunk(pFloat, FRAMES_PER_BUFFER)
+      arrayChunk(pByte, BYTES_PER_BUFFER)
     })
     .flatMap(Pull.output)
     .streamNoScope
     .repeat
 
-private def arrayChunk(pointer: Ptr[Float], length: Int): Chunk[Float] =
+private def arrayChunk(pointer: Ptr[Byte], length: Int): Chunk[Float] =
   val array = new Array[Float](length)
-  (0 until length).foreach(i => array(i) = pointer(i))
-  Chunk.array(array)
+  memcpy(array.atUnsafe(0).toBytePointer, pointer, length.toULong)
+  Chunk.ArraySlice(array, 0, length)
