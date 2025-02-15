bdc786de4aacf2226713b8fd34fedbfe8d1c86a4
Will Binzi <will.binzi@moia.io>
2025-02-15 16:13:42 +0100

    Format on compile

--- src/main/scala/pedals/chunk.scala
+++ src/main/scala/pedals/chunk.scala
@@ -4,15 +4,13 @@ import cats.Semigroup
 import cats.effect.Concurrent
 import fs2.{Chunk, Stream}
 
-implicit def streamPointwiseAddChunks[F[_]: Concurrent]
-    : Semigroup[Stream[F, Chunk[Float]]] =
-  new Semigroup[Stream[F, Chunk[Float]]]:
-    def combine(
-        x: Stream[F, Chunk[Float]],
-        y: Stream[F, Chunk[Float]]
-    ): Stream[F, Chunk[Float]] =
-      // Note: for some reason parZipWith doesn't work with JVM implementation
-      x.parZipWith(y)(_ |+| _)
+given streamPointwiseAddChunks[F[_]: Concurrent]
+    : Semigroup[Stream[F, Chunk[Float]]] = new:
+  def combine(
+      x: Stream[F, Chunk[Float]],
+      y: Stream[F, Chunk[Float]]
+  ): Stream[F, Chunk[Float]] =
+    x.parZipWith(y)(_ |+| _)
 
 extension (chunk: Chunk[Float])
   // Pointwise addition of two chunks
