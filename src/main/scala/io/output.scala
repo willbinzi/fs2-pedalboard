e8e8715edfb919060ec40acc7d2d56707b8ca8f1
willbinzi <54801276+willbinzi@users.noreply.github.com>
2025-02-15 16:02:02 +0100

    Simplify output pipe (#16)

--- src/main/scala/io/output.scala
+++ src/main/scala/io/output.scala
@@ -12,12 +12,13 @@ import scala.scalanative.unsigned.UnsignedRichInt
 def outputPipeFromPointer[F[_]](pStream: Ptr[PaStream])(implicit
     F: Sync[F]
 ): Pipe[F, Float, Nothing] =
-  val pFloat: Ptr[Float] = stackalloc[Float](FRAMES_PER_BUFFER)
-  val pByte: Ptr[Byte] = pFloat.toBytePointer
+  // Scala native 0.4 is single threaded so we can re-use the same buffer
+  // Once this project moves to use multithreading, this will no longer be possible
+  val buffer = new Array[Float](FRAMES_PER_BUFFER)
   _.chunks.foreach { chunk =>
     F.blocking {
-      (0 until chunk.size).foreach(i => pFloat(i) = chunk(i))
-      functions.Pa_WriteStream(pStream, pByte, FRAMES_PER_BUFFER.toULong)
+      chunk.copyToArray(buffer, 0)
+      functions.Pa_WriteStream(pStream, buffer.atUnsafe(0).toBytePointer, FRAMES_PER_BUFFER.toULong)
       ()
     }
   }
