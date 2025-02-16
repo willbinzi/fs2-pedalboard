3848a002c1de3681e8534f7d600f364365e4efe6
Will Binzi <will.binzi@moia.io>
2025-02-16 13:04:01 +0100

    Emphasize difference between input and output buffer

--- src/main/scala/io/portaudio/PortAudioAudioSuite.scala
+++ src/main/scala/io/portaudio/PortAudioAudioSuite.scala
@@ -12,36 +12,37 @@ import scala.scalanative.unsigned.UnsignedRichInt
 
 object PortAudioAudioSuite:
   def apply[F[_]](using F: Sync[F]): Resource[F, AudioSuite[F]] =
+    // Note: This is not thread safe!
+    // Scala native 0.4 is single threaded so we can re-use the same input and output buffer each time
+    // Once this project moves to use multithreading, this will no longer be possible
+    val inputBuffer  = new Array[Float](FRAMES_PER_BUFFER)
+    val outputBuffer = new Array[Float](FRAMES_PER_BUFFER)
+
     for {
       _ <- initPortaudio[F]
       pStream <- inputOutputStreamPointer[F]
     } yield new AudioSuite[F]:
       def input: Stream[F, Float] =
-        // Scala native 0.4 is single threaded so we can re-use the same buffer
-        // Once this project moves to use multiprocessing, this will no longer be possible
-        val buffer = new Array[Float](FRAMES_PER_BUFFER)
         Pull
           .eval(F.blocking {
             functions.Pa_ReadStream(
               stream = pStream,
-              buffer = buffer.atUnsafe(0).toBytePointer,
+              buffer = inputBuffer.atUnsafe(0).toBytePointer,
               frames = FRAMES_PER_BUFFER.toULong
             )
-            Chunk.ArraySlice(buffer, 0, FRAMES_PER_BUFFER)
+            Chunk.ArraySlice(inputBuffer, 0, FRAMES_PER_BUFFER)
           })
           .flatMap(Pull.output)
           .streamNoScope
           .repeat
 
       def output: Pipe[F, Float, Nothing] =
-        // As above, we can share the same buffer until the project moves to use multiprocessing
-        val buffer = new Array[Float](FRAMES_PER_BUFFER)
         _.chunks.foreach { chunk =>
           F.blocking {
-            chunk.copyToArray(buffer, 0)
+            chunk.copyToArray(outputBuffer, 0)
             functions.Pa_WriteStream(
               stream = pStream,
-              buffer = buffer.atUnsafe(0).toBytePointer,
+              buffer = outputBuffer.atUnsafe(0).toBytePointer,
               frames = FRAMES_PER_BUFFER.toULong
             )
             ()
