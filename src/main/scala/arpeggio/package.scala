e9efb48e5518f14432aa5cca729ad5d8a93ac009
Will Binzi <will.binzi@moia.io>
2025-02-17 19:50:13 +0100

    Enforce correct chunk size in output buffer, adjust pointwiseAdd to be agnostic of chunk size and rewrite parallel to accomodate it

--- src/main/scala/arpeggio/package.scala
+++ src/main/scala/arpeggio/package.scala
@@ -1,20 +1,14 @@
 package arpeggio
 
 import cats.Semigroup
-import cats.effect.Concurrent
-import fs2.{Chunk, Pipe, Stream}
+import fs2.{Pipe, Stream}
 
 type Pedal[F[_]] = Pipe[F, Float, Float]
 
-given streamPointwiseAddChunks[F[_]: Concurrent]: Semigroup[Stream[F, Float]] =
+given pointwiseAdd[F[_]]: Semigroup[Stream[F, Float]] =
   new:
     def combine(
         x: Stream[F, Float],
         y: Stream[F, Float]
     ): Stream[F, Float] =
-      x.chunks.parZipWith(y.chunks)(_ |+| _).unchunks
-
-extension (chunk: Chunk[Float])
-  // Pointwise addition of two chunks
-  def |+|(other: Chunk[Float]): Chunk[Float] =
-    chunk.zipWith(other)(_ + _)
+      x.zipWith(y)(_ + _)
--- src/main/scala/arpeggio/routing/parallel.scala
+++ src/main/scala/arpeggio/routing/parallel.scala
@@ -1,44 +1,22 @@
 package arpeggio
 package routing
 
+import arpeggio.pubsub.ChunkedTopic.*
 import cats.data.NonEmptySeq
-import cats.effect.std.CountDownLatch
 import cats.effect.Concurrent
-import cats.syntax.flatMap.*
-import cats.syntax.functor.*
-import fs2.concurrent.Topic
-import fs2.{Chunk, Stream}
+import cats.syntax.traverse.toTraverseOps
+import fs2.Stream
 
 def parallel[F[_]: Concurrent](pedals: Pedal[F]*): Pedal[F] =
-  NonEmptySeq.fromSeq(pedals).fold[Pedal[F]](_.map(_ * 0))(parallelNonEmpty)
+  assert(pedals.nonEmpty, s"Cannot run 0 pedals in parallel")
+  parallelNonEmpty(NonEmptySeq.fromSeqUnsafe(pedals))
 
-// Adapted from implementation of broadcastThrough from fs2:
-// https://github.com/typelevel/fs2/blob/f8105b7a22842e4f97d188fcf8f11283c2f617e2/core/shared/src/main/scala/fs2/Stream.scala#L239
 def parallelNonEmpty[F[_]: Concurrent](pedals: NonEmptySeq[Pedal[F]]): Pedal[F] =
-  stream => {
-    Stream.force {
-      for {
-        // topic: contains the chunk that the pipes are processing at one point.
-        // until and unless all pipes are finished with it, won't move to next one
-        topic <- Topic[F, Chunk[Float]]
-        // Coordination: neither the producer nor any consumer starts
-        // until and unless all consumers are subscribed to topic.
-        allReady <- CountDownLatch[F](pedals.length)
-      } yield {
-        val checkIn = allReady.release >> allReady.await
-
-        def dump(pipe: Pedal[F]): Stream[F, Float] =
-          Stream.resource(topic.subscribeAwait(1)).flatMap { sub =>
-            // Wait until all pipes are ready before consuming.
-            // Crucial: checkin is not passed to the pipe,
-            // so pipe cannot interrupt it and alter the latch count
-            Stream.exec(checkIn) ++ pipe(sub.unchunks)
-          }
-
-        val dumpAll: Stream[F, Float] = pedals.map(dump).reduce
-        // Wait until all pipes are checked in before pulling
-        val pump = Stream.exec(allReady.await) ++ topic.publish(stream.chunks)
-        dumpAll.concurrently(pump)
-      }
-    }
-  }
+  stream =>
+    for {
+      topic <- Stream.eval(ChunkedTopic[F, Float])
+      pedalOutputs <- Stream.resource(
+        pedals.traverse(p => topic.subscribeAwait.map(p))
+      )
+      result <- pedalOutputs.reduce.concurrently(stream.through(topic.publish))
+    } yield result
