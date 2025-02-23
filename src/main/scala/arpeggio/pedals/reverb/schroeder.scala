477931c9d939d383470e2241bba06a86be318bd3
Will Binzi <will.binzi@moia.io>
2025-02-23 12:36:09 +0100

    Simplify reverb so we don't have to normalise the chunk size - lowered sample rate due to reduced performance

--- src/main/scala/arpeggio/pedals/reverb/schroeder.scala
+++ src/main/scala/arpeggio/pedals/reverb/schroeder.scala
@@ -6,28 +6,28 @@ import arpeggio.routing.parallel
 import cats.effect.Concurrent
 
 def schroeder[F[_]: Concurrent](
-    predelayTimeInMs: Float, // = 30
-    reverbTimeInMs: Float
+    predelayMillis: Float,
+    decayMillis: Float
 ): Pedal[F] =
-  val t1 = predelayTimeInMs
-  val t2 = predelayTimeInMs * 1.17f
-  val t3 = predelayTimeInMs * 1.34f
-  val t4 = predelayTimeInMs * 1.5f
+  val t1 = predelayMillis
+  val t2 = predelayMillis * 1.17f
+  val t3 = predelayMillis * 1.34f
+  val t4 = predelayMillis * 1.5f
   parallel(
     identity,
     parallel(
-      echoRepeats(gain(reverbTimeInMs, t1), t1),
-      echoRepeats(gain(reverbTimeInMs, t2), t2),
-      echoRepeats(gain(reverbTimeInMs, t3), t3),
-      echoRepeats(gain(reverbTimeInMs, t4), t4)
+      echoRepeats(gain(decayMillis, t1), t1),
+      echoRepeats(gain(decayMillis, t2), t2),
+      echoRepeats(gain(decayMillis, t3), t3),
+      echoRepeats(gain(decayMillis, t4), t4)
     ).andThen(
       allPassStage(0.7, 5)
         .andThen(allPassStage(0.7, 1.7))
     )
   )
 
-def gain(reverbTimeInMs: Float, predelayTimeInMs: Float): Float =
-  scala.math.pow(2, (-3f * predelayTimeInMs) / reverbTimeInMs).toFloat
+def gain(decayMillis: Float, predelayMillis: Float): Float =
+  scala.math.pow(2, (-3f * predelayMillis) / decayMillis).toFloat
 
 def allPassStage[F[_]: Concurrent](
     repeatGain: Float,
