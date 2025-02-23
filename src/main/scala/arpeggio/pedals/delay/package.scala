81caf6dd8dc10fcee19fdef6e90e72a2f446fb02
Will Binzi <will.binzi@moia.io>
2025-02-23 18:57:47 +0100

    Extract buffering used for delayLine into a more general function

--- src/main/scala/arpeggio/pedals/delay/package.scala
+++ src/main/scala/arpeggio/pedals/delay/package.scala
@@ -13,15 +13,17 @@ def silence[F[_]](timeInMillis: Float): Stream[F, Float] =
   val timeInFrames = (timeInMillis * SAMPLE_RATE / 1000).toInt
   Stream.chunk(Chunk.constant(0, timeInFrames))
 
-def delayLine[F[_]: Concurrent](timeInMillis: Float): Pedal[F] = stream =>
+def buffered[F[_]: Concurrent](pedal: Pedal[F]): Pedal[F] = stream =>
   Stream
     .eval(ChunkedChannel.unbounded[F, Float])
     .flatMap(channel =>
-      channel.stream.concurrently(
-        (silence(timeInMillis) ++ stream)
-          .through(channel.sendAll)
-      )
+      channel.stream
+        .through(pedal)
+        .concurrently(stream.through(channel.sendAll))
     )
+
+def delayLine[F[_]: Concurrent](timeInMillis: Float): Pedal[F] =
+  buffered(silence(timeInMillis) ++ _)
 
 def echo[F[_]: Concurrent](
     repeatGain: Float,
